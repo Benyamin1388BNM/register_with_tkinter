65c861918e4cb29bc74e63164fadb59bf4c1cb8c
Benyamin1388BNM <benyaminaraste3@gmail.com>
2024-11-01 21:02:49 +0330

    Update register_tkinter.py

--- register_tkinter.py
+++ register_tkinter.py
@@ -5,7 +5,8 @@ window=Tk()
 window.title('login_member')
 window.geometry('600x600')
 window.config(bg='pink')
-def check_information(entry_register,entry_username,entry_password):
+def check_information():
+    global entry_register,entry_username,entry_password
     register=entry_register.get()
     username=entry_username.get()
     password=entry_password.get()
@@ -17,7 +18,9 @@ def check_information(entry_register,entry_username,entry_password):
             username_password=Tuple[0]
             if password==username_password:
                 messagebox.showinfo('welcome','information was True')
-                window.destroy()
+                entry_register.delete(0,END)
+                entry_username.delete(0,END)
+                entry_password.delete(0,END)
             else:
                 messagebox.showerror('sorry','password was false')
         except :
@@ -36,22 +39,26 @@ def check_information(entry_register,entry_username,entry_password):
                     mycursor.executemany('INSERT INTO members(username,password) VALUES (%s,%s)',values)
                     conn.commit()
                     messagebox.showinfo('welcome','welcome to our family')
-                    window.destroy()
+                    entry_register.delete(0,END)
+                    entry_username.delete(0,END)
+                    entry_password.delete(0,END)
                 else:
                     messagebox.showerror('password','password is not safe')
         else:
             messagebox.showerror('sorry','username should be greater from 2 letters')
     else:
         messagebox.showerror('Sorry',"I don't underestand your mean! Are you registerid?")
-def see(type_see):
+def see():
+    global my_button
+    global password_button
     global entry_password
-    if type_see=='yes':
-        entry_password=Entry(window,font=('arial',16,'bold'))
-        entry_password.grid(row=3,column=2)
-    if type_see=='no':
-        entry_password=Entry(window,show='*',font=('arial',16,'bold'))
-        entry_password.grid(row=3,column=2)
-text_register=Label(window,text='Are you register?',font=('arial',16,'bold')).grid(row=1,column=1)
+    if entry_password.cget('show')=="":
+        entry_password.config(show='*')
+        password_button.config(text='show password')
+    else:
+        entry_password.config(show="")
+        password_button.config(text='hide password')
+text_register=Label(window,bg='green',fg='white',text='Are you register?',font=('arial',16,'bold')).grid(row=1,column=1)
 text_username=Label(window,text='username:',font=('arial',16,'bold')).grid(row=2,column=1)
 text_password=Label(window,text='password:',font=('arial',16,'bold')).grid(row=3,column=1)
 entry_register=Entry(window,font=('arial',16,'bold'))
@@ -60,7 +67,10 @@ entry_username=Entry(window,font=('arial',16,'bold'))
 entry_username.grid(row=2,column=2)
 entry_password=Entry(window,show='*',font=('arial',16,'bold'))
 entry_password.grid(row=3,column=2)
-my_button=Button(window,text='OK',font=('arial',16,'bold'),command=lambda:check_information(entry_register,entry_username,entry_password)).grid(row=4,column=3)
-my_button2=Button(window,text='see',font=('arial',14,'bold'),command=lambda:see('yes')).grid(row=3,column=3)
-my_button2=Button(window,text='safe',font=('arial',14,'bold'),command=lambda:see('no')).grid(row=3,column=4)
+my_button=Button(window,text='OK',bg='orange',fg='white',font=('arial',16,'bold'),command=check_information)
+my_button.grid(row=4,column=3)
+password_button=Button(window,text='show paswsword',font=('arial',14,'bold'),command=see)
+password_button.grid(row=3,column=3)
+exit_button=Button(window,text='EXIT',font=('arial',16,'bold'),bg='red',fg='white',command=lambda :window.destroy())
+exit_button.grid(row=1,column=3)
 window.mainloop()
